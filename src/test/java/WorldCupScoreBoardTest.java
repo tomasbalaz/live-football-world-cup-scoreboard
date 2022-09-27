634251a8d38ba8a8beee5d8f749b72cd929efb39
Tomas Balaz <balaztomas0@gmail.com>
2022-09-27 12:58:34 +0200

    Created WorldCupScoreBoardTest with junit 5 dependencies

---
+++ src/test/java/WorldCupScoreBoardTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class WorldCupScoreBoardTest {
+
+    private ScoreBoard underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new ScoreBoard();
+    }
+
+    @Test
+    void itShouldInsertMatch() {
+
+    }
+}
